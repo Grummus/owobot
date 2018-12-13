4d8fd38d59d16de8df7d2208c95a0e8c062c61a9
Graham Klatt <33434467+Grummus@users.noreply.github.com>
2018-12-12 21:23:50 -0800

    Forbidden timer added!
    OwO Bot now times how long servers can go without saying the forbidden word!

--- bot.js
+++ bot.js
@@ -2,6 +2,11 @@ var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
 var bulgecount = 0;
+
+var startTime, endTime;
+var time;
+var seconds;
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -17,7 +22,10 @@ bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
+    startTimer();
+    console.log("Ready!");
 });
+
 bot.on('message', function (user, userID, channelID, message, evt) {
 	
     // OwO bot needs to know if it will execute a command
@@ -38,12 +46,15 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 			case 'bulgecount':
 				bot.sendMessage({
 					to: channelID,
-					message: bulgecount + ' bulges noticed!'
-				})
+                    message: bulgecount + ' Bulges noticed!'
+                })
+                bulgecount--;
 			break;
         }
     }
-	
+
+    var messageLowercase = message.toLowerCase();
+
 	if (message.includes("bulge")) {
 		bot.sendMessage({
 			to: channelID,
@@ -51,20 +62,63 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 		})
 		console.log('Bulge Noticed!');
 		bulgecount++;
-	}
+    }
+
+    if (messageLowercase.includes("whats this") || messageLowercase.includes("what's this")) {
+        bot.sendMessage({
+            to: channelID,
+            message: 'OwO?'
+        })
+    }
 	
-	if (message.includes("good bot")) {
+	if (messageLowercase.includes("good bot")) {
 		bot.sendMessage({
 			to: channelID,
 			message: 'UwU'
 		})
 	}
 	
-	if (message.includes("bad bot")) {
+	if (messageLowercase.includes("bad bot")) {
 		bot.sendMessage({
 			to: channelID,
 			message: 'ಥ_ಥ'
 		})
-	}
-	
+    }
+
+
+    if (messageLowercase.includes("fuck")) {
+        bot.sendMessage({
+            to: channelID,
+            message: 'ÒwÓ'
+        })
+    }
+
+    // So this part logs how long a server can go without saying the word 'vore'
+    if (messageLowercase.includes("vore")) {
+        seconds = stopTimer() / 1000;
+        var days = Math.floor(seconds / (3600*24));
+        seconds  -= days*3600*24;
+        var hrs   = Math.floor(seconds / 3600);
+        seconds  -= hrs*3600;
+        var mnts = Math.floor(seconds / 60);
+        seconds  -= mnts*60;
+
+        bot.sendMessage({
+            to: channelID,
+            message: user.toUpperCase() + ' HAS SPOKEN THE FORBIDDEN WORD!\nThis server has gone:\n' + days + ' days,\n' + hrs + ' hours,\n' + mnts + ' minutes, and\n' + seconds + ' seconds\nwithout saying the forbidden word!'
+        })
+        startTimer();
+    }
 });
+
+function startTimer() {
+    startTime = new Date();
+    console.log("Resetting Timer!");
+}
+
+function stopTimer() {
+    endTime = new Date();
+    endTime -= startTime;
+    console.log(endTime);
+    return endTime;
+}
