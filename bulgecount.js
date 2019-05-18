f9605e3d547928060d620d773f489449d934404c
Grummus <grahamklatt@msn.com>
2019-05-18 07:38:09 +0000

    perfected the bulgecount script

--- bulgecount.js
+++ bulgecount.js
@@ -1,8 +1,44 @@
 var colors = require('colors')
 var LocalStorage = require('node-localstorage').LocalStorage;
 localStorage = new LocalStorage('./localstorage');
+var cowsay = require('cowsay');
+var lolcatjs = require('lolcatjs');
 
 var globalBulgeCount = localStorage.getItem('globalBulges');
 var globalVoreCount = localStorage.getItem('globalVore');
 
-console.log(globalBulgeCount, 'bulges noticed globally!');
+exports.globalBulgeCount = globalBulgeCount;
+exports.globalVoreCount = globalVoreCount;
+
+
+lolcatjs.options.seed = 1000;
+lolcatjs.options.colors = true;
+lolcatjs.options.freq = 1;
+process.stdout.write('\033c');
+// process.stdout.write(globalBulgeCount + ' bulges noticed globally');
+	var said = cowsay.say({
+		text : globalBulgeCount + " bulges noticed globally",
+		e : "oO",
+		T : "U "
+	});
+	lolcatjs.fromString(said);
+loop();
+
+
+function loop() {
+	setTimeout( function() {
+	
+		globalBulgeCount = localStorage.getItem('globalBulges');
+		process.stdout.cursorTo(2, 1);
+		// process.stdout.write(globalBulgeCount);
+	//	console.log(cowsay.say({
+	//			text : globalBulgeCount + " bulges noticed globally",
+	//			e : "oO",
+	//			T : "U "
+	//	}));
+		process.stdout.write(globalBulgeCount);
+		process.stdout.cursorTo(0, 8);
+		loop();
+	}, 3000)
+}
+		// console.log(globalBulgeCount, 'bulges noticed globally!');
